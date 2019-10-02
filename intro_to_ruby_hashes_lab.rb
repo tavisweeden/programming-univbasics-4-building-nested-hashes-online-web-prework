851df68dd755bd317ee9e184fb4773588e58af85
Tavis Weeden <tavisweeden@gmail.com>
2019-10-02 02:56:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      :key8 => "value4"
+      :shortline_railroad => {}
       
     } } }
 
