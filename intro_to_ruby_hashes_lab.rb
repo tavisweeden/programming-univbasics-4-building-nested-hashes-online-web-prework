6fdf3ba4517f9d6f31f304117dc5b7aaa8fd4568
Tavis Weeden <tavisweeden@gmail.com>
2019-10-02 02:52:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
-      :key3 => 3,
+      :three_pieces_owned => 100,
       :key4 => 4
       
     },
